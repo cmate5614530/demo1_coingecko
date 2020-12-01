bf92c894092ddd479786169d9df5cbb4fef35121
cmate5614530 <cmate5614530@gmail.com>
2020-12-01 16:07:11 +0300

    modify some features

---
+++ README.md
@@ -0,0 +1,18 @@
+# Demo Coingecko 
+
+Multi-language Ethereum price in the Bitcoin and in the USD, where the price would be updated every 40 seconds
+
+## Features
+
+- NodeJS/Express
+- VueJS
+- EJS
+
+## Installation
+
+- clone the repository
+- npm install
+- To run the project 
+    npm start
+    * To run the project with development nodemon: npm run dev
+- you can see the page on 'http://localhost:5000'
--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cors = require('cors');
-//var cookieParser = require('cookie-parser');
-//var logger = require('morgan');
 var engine = require('ejs-locals');
 var indexRouter = require('./routes/index');
 var app = express();
@@ -14,10 +12,8 @@ app.engine('ejs', engine)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
